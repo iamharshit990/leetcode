0363f8005d5d55363c4fee69f078ec9ed37b74b8
iamharshit990 <iamharshit990@gmail.com>
2026-07-02 17:35:43 +0530

    Time: 15 ms (27.90%) | Memory: 51.5 MB (88.76%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans=new ArrayList<>();
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            int key=entry.getKey();
+            int value=entry.getValue();
+            if(value>(int)nums.length/3){
+                ans.add(key);
+            }
+        }
+        return ans;
+        
+    }
+}
