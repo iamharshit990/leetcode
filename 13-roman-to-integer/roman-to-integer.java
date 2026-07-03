95f54f2f90b43ad77350da0b2898d7a4c67bc699
iamharshit990 <iamharshit990@gmail.com>
2026-07-03 20:13:00 +0530

    Time: 3 ms (78.14%) | Memory: 46.1 MB (96.90%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int romanToInt(String s) {
+        int val = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            int current = getValue(s.charAt(i));
+            
+            // Check if there is a next character and if current < next
+            // If it is, we subtract it (like IV or IX)
+            if (i + 1 < n && current < getValue(s.charAt(i + 1))) {
+                val -= current;
+            } else {
+                // Otherwise, add it normally
+                val += current;
+            }
+        }
+        return val;
+    }
+    
+    private int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
