0116f3a1386783fb0ccd61e8ff009c0da037d009
iamharshit990 <iamharshit990@gmail.com>
2026-07-06 00:17:49 +0530

    Time: 1 ms (99.96%) | Memory: 77.4 MB (19.21%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,13 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //kadanes algo
+        int cans=nums[0];
         int ans=nums[0];
-        int cs=nums[0];
         for(int i=1;i<nums.length;i++){
-            int v1=cs+nums[i];
+            int v1=cans+nums[i];
             int v2=nums[i];
-            cs=Math.max(v1,v2);
-            ans=Math.max(ans,cs);
+            cans=Math.max(v1,v2);
+            ans=Math.max(cans,ans);
         }
         return ans;
         
