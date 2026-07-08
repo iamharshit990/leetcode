5fa5822b5ebd06f91337660bef7bae7ab580e2cb
iamharshit990 <iamharshit990@gmail.com>
2026-07-08 22:23:39 +0530

    Time: 1 ms (98.88%) | Memory: 47.5 MB (88.92%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,45 +1,19 @@
+//optimised
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if(n==0) return true;
-        if(flowerbed.length==1){
-            if(flowerbed[0]==1) return false;
-            else return true;
-           
-        }
-        if(flowerbed.length<2){
-            return false;
-        }
-        
-        
-        
-        int count =0;
-        for(int i=0;i<flowerbed.length;i++){
-            if(i==0){
-                if(flowerbed[i]==0&&flowerbed[i+1]==0){
-                    count ++;
-                    flowerbed[i]=1;
-                }
-            }
-            else if(i==flowerbed.length-1){
-                if(flowerbed[i]==0&&flowerbed[i-1]==0){
-                    count ++;
-                    flowerbed[i]=1;
-                }
-            }
-            else{
-                if(flowerbed[i-1]==0&&flowerbed[i+1]==0&&flowerbed[i]==0){
-                count ++;
-                flowerbed[i]=1;
-                }
-                
-            }
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                // Check if left and right are empty (or out of bounds)
+                boolean leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean rightEmpty = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
 
+                if (leftEmpty && rightEmpty) {
+                    flowerbed[i] = 1; // Plant it!
+                    count++;
+                }
+            }
         }
-        if(n<=count){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return count >= n;
     }
 }
