d20b3dec1ba2aedd60bf1adac902c2f1c0905c76
iamharshit990 <iamharshit990@gmail.com>
2026-07-18 16:27:27 +0530

    Time: 5 ms (13.98%) | Memory: 43.5 MB (15.85%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character>stack= new Stack<>();
-        //"([)]"
+        Stack<Character> stack =  new Stack<>();
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
             if(c=='('||c=='{'||c=='['){
@@ -12,21 +11,17 @@ class Solution {
                     if(stack.isEmpty()||stack.peek()!='(') return false;
                     stack.pop();
                 }
-                else if(c=='}'){
+                if(c=='}'){
                     if(stack.isEmpty()||stack.peek()!='{') return false;
                     stack.pop();
-                    
                 }
-                else {
+                if(c==']'){
                     if(stack.isEmpty()||stack.peek()!='[') return false;
                     stack.pop();
-
                 }
             }
         }
         if(stack.isEmpty()) return true;
         else return false;
-
-        
     }
 }
