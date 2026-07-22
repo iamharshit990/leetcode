d3c719ac179fe4b061b6e3788f8c4e78c1f976c7
iamharshit990 <iamharshit990@gmail.com>
2026-07-22 17:26:07 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (99.49%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        // brute 
-        int ans1=Integer.MAX_VALUE;
-        int ans2=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                ans1=Math.min(ans1,i);
-                ans2=Math.max(ans2,i);
+        int low=0;
+        int high=nums.length-1;
+        int ans1=-1;
+        int ans2=-1;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if(nums[mid]==target){
+                ans1=mid;
+                high=mid-1;
             }
+            else if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
         }
-        if(ans1==Integer.MAX_VALUE||ans2==Integer.MIN_VALUE){
-            return new int []{-1,-1};
+        low=0;
+        high=nums.length-1;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if(nums[mid]==target){
+                ans2=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
+        }
+        if(ans1==-1||ans2==-1){
+            return new int[]{-1,-1};
         }
         return new int[]{ans1,ans2};
-        
+
         
     }
 }
