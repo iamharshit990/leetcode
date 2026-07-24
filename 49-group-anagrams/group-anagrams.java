48df5c29eede29e2c7ffc68e96efbb84d66107ee
iamharshit990 <iamharshit990@gmail.com>
2026-07-24 18:05:39 +0530

    Time: 7 ms (73.68%) | Memory: 49.3 MB (96.20%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,27 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<HashMap<Character,Integer>,List<String>> map= new HashMap<>();
-        for(String s: strs){
-            HashMap<Character,Integer> curr= new HashMap<>();
-            for(int i=0;i<s.length();i++){
-                curr.put(s.charAt(i),curr.getOrDefault(s.charAt(i),0)+1);
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s:strs){
+            char arr[] = s.toCharArray();
+            Arrays.sort(arr);
+            String str = new String(arr);
+            if(!map.containsKey(str)){
+                map.put(str,new ArrayList<>());
             }
-            if(map.containsKey(curr)){
-                List<String> now = map.get(curr);
-                now.add(s);
-                map.put(curr,now);
-            }
-            else{
-                List<String> meow= new ArrayList<>();
-                meow.add(s);
-                map.put(curr,meow);
-            }
+            map.get(str).add(s);
+
         }
-        List<List<String>> ans= new ArrayList<>();
-        for(Map.Entry<HashMap<Character,Integer>, List<String>> e : map.entrySet()){
-            List<String> group = e.getValue();
-            ans.add(group);
-        }
-        return ans;
+        return new ArrayList<>(map.values());
+
+        
     }
 }
