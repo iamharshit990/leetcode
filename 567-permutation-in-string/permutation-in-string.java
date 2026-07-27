3150cf9a733c6d9f9abcefd311c64afcf35da0a6
iamharshit990 <iamharshit990@gmail.com>
2026-07-28 00:12:13 +0530

    Time: 6 ms (88.03%) | Memory: 43.8 MB (85.83%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,31 +1,45 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length()) return false;
-        int low=0;
-        int high=s1.length();
-        int[] arr1= new int[26];
-        for(int i=0;i<s1.length();i++){
-            arr1[s1.charAt(i) -'a']++;
+        if (s1.length() > s2.length())
+            return false;
+        int low = 0;
+        int high = s1.length();
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            arr1[s1.charAt(i) - 'a']++;
         }
-        while(high<=s2.length()){
-            int arr2[]= new int[26];
-            boolean isMatch= true;
-            for(int i=low;i<high;i++){
-                arr2[s2.charAt(i)-'a']++;
+        for (int i = 0; i < s1.length(); i++) {
+            arr2[s2.charAt(i) - 'a']++;
+        }
+        boolean isSame = true;
+
+        for (int i = 0; i < 26; i++) {
+            if (arr1[i] != arr2[i]) {
+                isSame = false;
             }
-            int index=0;
-            for(int i=0;i<26;i++){
-                if(arr1[i]!=arr2[i]){
-                    isMatch=false;
-                    low++;
-                    high++;
+        }
+        if (isSame)
+            return true;
+
+        while (high < s2.length()) {
+            arr2[s2.charAt(high) - 'a']++;
+            arr2[s2.charAt(low) - 'a']--;
+            low++;
+            high++;
+
+            boolean isMatch = true;
+            for (int i = 0; i < 26; i++) {
+                if (arr1[i] != arr2[i]) {
+                    isMatch = false;
+
                     break;
                 }
             }
-            if(isMatch) return true;
+            if (isMatch)
+                return true;
         }
         return false;
 
-        
     }
 }
