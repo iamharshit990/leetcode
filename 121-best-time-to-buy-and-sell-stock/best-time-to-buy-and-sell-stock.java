e07f14b4e56374671b298ffafe4e0c305a504375
iamharshit990 <iamharshit990@gmail.com>
2026-07-27 23:09:04 +0530

    Time: 2 ms (42.30%) | Memory: 94 MB (94.39%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //brute is n2
-        // this is sub optimal O(n) and space O(n)
-        int[] right= new int[prices.length];
-        right[right.length-1]=prices[prices.length-1];
-        for(int i=prices.length-2;i>=0;i--){
-            right[i]=Math.max(prices[i],right[i+1]);
-        }
-        int ans=0;
-        for(int i=0;i<prices.length;i++){
-            int curr = right[i] - prices[i];
-            ans=Math.max(ans,curr);
+      //optimal2
+      if(prices.length==1) return 0;
+      int min = prices[0];
+      int  ans=0;
+      for(int i=1;i<prices.length;i++){
+        int curr= prices[i]-min;
+        ans=Math.max(ans,curr);
+        min=Math.min(prices[i],min);
+      }
+      return ans;
 
-        }
-        return ans;
+        
     }
 }
