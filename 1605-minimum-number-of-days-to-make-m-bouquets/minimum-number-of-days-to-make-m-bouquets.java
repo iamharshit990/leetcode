8cf4c101eaf6a48945b7c113ef6321a6426a7243
iamharshit990 <iamharshit990@gmail.com>
2026-07-30 03:02:29 +0530

    Time: 28 ms (5.65%) | Memory: 82.9 MB (12.97%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean helper(int [] arr, int m , int k,long mid){
+        //1 10 3 10 2   5
+        int nb=0;
+        long count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<mid){
+                count++;
+            }
+            else {
+                count=0;
+            }
+            if(count==k) {
+                nb++;
+                count=0;
+            }
+
+        }
+        if(nb>=m) return true;
+        else return false;
+
+    }
+    public int minDays(int[] arr, int m, int k) {
+        // arr m--> no of boutiques 
+        // we need to use k adjacent to make a boutique i.e 2 2 ka pair will give 1 so we need to make m boutiques
+        // to make m we would need k no of adjacent 
+        long low=1;
+        long high=0;
+        for(int i:arr){
+            high+=i;
+        }
+        int ans=-1;
+        while(low<=high){
+            long mid=(low+high)/2;
+            if(helper(arr,m,k,mid)){
+                ans=(int)mid;
+                high=(mid)-1;
+            }
+            else {
+                low=(int)(mid)+1;
+            }
+        }
+        if(ans>0) return ans-1;
+        else return ans;
+
+
+        
+    }
+}
