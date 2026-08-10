e898537e86d16551041dc1df1fd5e75279ea98d3
iamharshit990 <iamharshit990@gmail.com>
2026-08-10 13:29:12 +0530

    Time: 44 ms (100.00%) | Memory: 146.1 MB (100.00%) - LeetSync

--- 4363-minimum-total-price-after-applying-discounts/minimum-total-price-after-applying-discounts.java
+++ 4363-minimum-total-price-after-applying-discounts/minimum-total-price-after-applying-discounts.java
@@ -1,22 +1,15 @@
 class Solution {
     public double minPrice(int[] prices, int[] discounts) {
-        PriorityQueue<Integer> price = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> discount = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i : prices) {
-            price.add(i);
+        Arrays.sort(prices);
+        Arrays.sort(discounts);
+        double total = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (i < discounts.length) {
+                total += prices[prices.length - 1 - i] * (100 - discounts[discounts.length - 1 - i]) / 100.;
+            } else {
+                total += prices[prices.length - 1 - i];
+            }
         }
-        for (int i : discounts) {
-            discount.add(i);
-        }
-        int n = discount.size();
-        double ans = 0;
-        while (!discount.isEmpty() && !price.isEmpty()) {
-            ans += price.poll() * ((double) (100 - discount.poll()) / 100);
-        }
-        while (!price.isEmpty()) {
-            ans += price.poll();
-        }
-
-        return ans;
+        return total;
     }
 }
