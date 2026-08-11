b3f12d60f07954300977cef1d5768ed6243672f6
iamharshit990 <iamharshit990@gmail.com>
2026-08-11 23:39:12 +0530

    Time: 7 ms (49.33%) | Memory: 51.1 MB (61.67%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,32 +1,26 @@
 class Solution {
-    
-    public static int kadane(int arr[]){
-        int ans=arr[0];
-        int cans=arr[0];
-        for(int i=1;i<arr.length;i++){
-            cans=Math.max(cans+arr[i],arr[i]);
-            ans=Math.max(ans,cans);
+    public int min(int arr[]){
+        int sum=0;
+        for(int i:arr){
+            sum+=i;
         }
-        return ans;
+        int currsum=arr[0];
+        int min=arr[0];
+        for(int i=1;i<arr.length;i++){
+            currsum=Math.min(arr[i],currsum+arr[i]);
+            min=Math.min(currsum,min);
+        }
+        return sum-min;
 
     }
-    public static int circularkadane(int arr[]){
-        int ans=arr[0];
-        int cans=arr[0];
-        for(int i=1;i<arr.length;i++){
-            cans=Math.min(cans+arr[i],arr[i]);
-            ans=Math.min(ans,cans);
-        }
-        return ans;
-    }
     public int maxSubarraySumCircular(int[] nums) {
-        int sum = 0;
-        for (int x : nums) sum += x;
-        int v1=kadane(nums);
-        int v2=sum-circularkadane(nums);
-        if (v1 < 0) return v1;  // all negative, v2 would be invalid (0)
-        return Math.max(v1, v2);
-        
-        
+        int sum=nums[0];
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            sum=Math.max(nums[i],sum+nums[i]);
+            max=Math.max(sum,max);
+        }
+        if(max<0) return max;
+        return Math.max(max,min(nums));
     }
 }
