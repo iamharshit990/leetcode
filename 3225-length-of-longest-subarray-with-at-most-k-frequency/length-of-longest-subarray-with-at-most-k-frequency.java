68f6db6da55e14fcc6781314eceeba138474b42a
iamharshit990 <iamharshit990@gmail.com>
2026-08-12 23:27:07 +0530

    Time: 82 ms (8.45%) | Memory: 111.2 MB (8.62%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,20 +1,21 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        HashMap<Integer,Integer> map= new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         int low=0;
         int high=0;
         int ans=0;
         while(high<nums.length){
-            map.put(nums[high],map.getOrDefault(nums[high],0)+1);
-            while(map.get(nums[high])>k){
-                map.put(nums[low],map.get(nums[low])-1);
-                //if(map.get(nums[low])==0) map.remove(nums[low]);
-                low++;
+            int ch= nums[high];
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            while(map.get(ch)>k){
+                if(map.containsKey(nums[low])){
+                    map.put(nums[low],map.get(nums[low])-1);
+                    if(map.get(nums[low])==0) map.remove(nums[low]);
+                    low++;
+                }
             }
-            ans=Math.max(ans,high-low+1);
+            ans=Math.max(ans,(high-low+1));
             high++;
-
-
         }
         return ans;
         
