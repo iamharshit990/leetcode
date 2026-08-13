bed768e55b001d501a4a7bf06905d1c8a1afa86e
iamharshit990 <iamharshit990@gmail.com>
2026-08-13 18:17:36 +0530

    Time: 44 ms (20.12%) | Memory: 47.8 MB (66.45%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,48 +1,33 @@
+class Pair{
+    int el;
+    int diff;
+    Pair(int a, int b){
+        el=a;
+        diff=b;
+    }
+}
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-       int low=0;
-       int high=arr.length-1;
-       int ans=arr.length;
-       while(low<=high){
-        int mid=low+(high-low)/2;
-        if(arr[mid]>=x){
-            ans=mid;
-            high=mid-1;
-        }
-        else low=mid+1;
-
-       }
-       int i=ans-1;
-       int j=ans;
-       List<Integer> list = new ArrayList<>();
-       while(list.size()<k){
-        if(i<0) {
-            list.add(arr[j]);
-            j++;
-        }
-        else if(j==arr.length){
-            list.add(arr[i]);
-            i--;
-        }
-        else {
-            int diff1=Math.abs(arr[i]-x);
-            int diff2=Math.abs(arr[j]-x);
-            if(diff1>diff2){
-                list.add(arr[j]);
-                j++;
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            (a,b)->{
+                if(a.diff!=b.diff)
+                    return Integer.compare(b.diff,a.diff);
+                return Integer.compare(b.el,a.el);
             }
-            else if(diff1<diff2){
-                list.add(arr[i]);
-                i--;
-            }
-            else {
-                list.add(arr[i]);
-                i--;
+        );
+        for(int i=0;i<arr.length;i++){
+            int diff=Math.abs(x-arr[i]);
+            pq.add(new Pair(arr[i],diff));
+            if(pq.size()>k){
+                pq.poll();
             }
         }
+        List<Integer> list= new ArrayList<>();
+        while(!pq.isEmpty()){
+            list.add(pq.poll().el);
+        }
+        Collections.sort(list);
+        return list;
         
-       } 
-       Collections .sort(list);
-       return list;
     }
 }
