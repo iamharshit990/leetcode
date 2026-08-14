9762d3517b631f383bf9afaf504f3eb6e49e0664
iamharshit990 <iamharshit990@gmail.com>
2026-08-14 14:01:45 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (59.89%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -3,15 +3,13 @@ class Solution {
         int low=0;
         int high=0;
         int ans=0;
-        HashMap<Character,Integer> map = new HashMap<>();
+        int [] arr = new int[26];
         while(high<s.length()){
             char ch = s.charAt(high);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            while(map.get(ch)>2){
-                if(map.containsKey(s.charAt(low))){
-                    map.put(s.charAt(low),map.get(s.charAt(low))-1);
-                    low++;
-                }
+            arr[ch-'a']++;
+            while(arr[ch-'a']>2){
+                arr[s.charAt(low)-'a']--;
+                low++;
             }
             ans=Math.max(ans,high-low+1);
             high++;
