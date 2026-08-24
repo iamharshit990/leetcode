c578b4ebe303bf669353effcfe05832f38468e48
iamharshit990 <iamharshit990@gmail.com>
2026-08-24 20:58:57 +0530

    Time: 1 ms (80.89%) | Memory: 43.7 MB (62.91%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -19,21 +19,20 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        boolean ltor= true;
+        boolean ltor = true;
         while(!q.isEmpty()){
             int size = q.size();
             List<Integer> temp = new ArrayList<>();
             while(size-->0){
                 TreeNode t = q.poll();
                 temp.add(t.val);
-                if(t.left!=null) q.add(t.left) ;
+                if(t.left!=null) q.add(t.left);
                 if(t.right!=null) q.add(t.right);
+
             }
-            // temp -> 3  and {9,20}  since we want 9,20 to be to right to left we can simply reverse it 
-            // we will use this boolean for that check 
             if(!ltor) Collections.reverse(temp);
-            ans.add(temp);
             ltor=!ltor;
+            ans.add(temp);
         }
         return ans;
         
