03c197c94b5436af50a5659b58d9a9feed0bcd8f
iamharshit990 <iamharshit990@gmail.com>
2026-09-05 11:06:34 +0530

    Time: 1 ms (86.40%) | Memory: 47.4 MB (31.65%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,11 +1,10 @@
 class Solution {
-    public void helper(int[][]adj, boolean[] vis, ArrayList<Integer> ans, int node) {
-        ans.add(node);
+    public void helper(int[][]adj, boolean[] vis, int node) {
         vis[node] = true;
         for (int i = 0; i < adj[node].length; i++) {
             int number = adj[node][i];
             if (adj[node][i]==1&&vis[i] == false) {
-                helper(adj, vis, ans, i);
+                helper(adj, vis, i);
             }
         }
         return;
@@ -13,12 +12,11 @@ class Solution {
 
     public int findCircleNum(int[][] adj) {
         int n = adj.length;
-        ArrayList<Integer> ans = new ArrayList<>();
         boolean vis [] = new boolean[n];
         int res = 0;
         for (int i = 0; i < n; i++) {
             if (!vis[i]){
-                helper(adj, vis, ans, i);
+                helper(adj, vis, i);
                 res++;
             }
         }
