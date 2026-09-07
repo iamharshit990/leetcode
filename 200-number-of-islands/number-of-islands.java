0d2c11048a84dfc23b5ff8149867fd2e09d77832
iamharshit990 <iamharshit990@gmail.com>
2026-09-07 17:01:50 +0530

    Time: 3 ms (87.84%) | Memory: 52.4 MB (45.16%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,17 +1,18 @@
 class Solution {
+    // without boolean arr
     int x[] = {-1,1,0,0};
     int y[] = {0,0,-1,1};
     public boolean valid(int i ,int j,int n,int m){
         if(i>=n||i<0||j>=m||j<0) return false;
         return true;
     }
-    public void helper(char[][]grid,boolean[][]vis,int i,int j,int n,int m){
-        vis[i][j] = true;
+    public void helper(char[][]grid,int i,int j,int n,int m){
+        grid[i][j]='2';
         for(int k=0;k<4;k++){
             int row = i+x[k];
             int col = j+y[k];
-            if(valid(row,col,n,m)&&vis[row][col]==false&&grid[row][col]=='1'){
-                helper(grid,vis,row,col,n,m);
+            if(valid(row,col,n,m)&&grid[row][col]=='1'){
+                helper(grid,row,col,n,m);
             }
         }
         return ;
@@ -20,12 +21,11 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m =grid[0].length;
-        boolean [][] vis = new boolean[n][m];
         int res = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j]&&grid[i][j]=='1'){
-                    helper(grid,vis,i,j,n,m);
+                if(grid[i][j]=='1'){
+                    helper(grid,i,j,n,m);
                     res++;
                 }
             }
