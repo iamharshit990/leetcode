b7480857c2d2ea85d788b8ca74cf4cd85db1a9ef
iamharshit990 <iamharshit990@gmail.com>
2026-09-08 13:07:03 +0530

    Time: 1 ms (99.34%) | Memory: 42.7 MB (55.63%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -5,7 +5,7 @@ class Solution {
         long base=1000;
         long commasUsed=0;
         while(base<=temp){
-            commasUsed+=(temp-base+1)*1;
+            commasUsed+=(temp-base+1);
             base*=1000;
         }
         return commasUsed;
