f15951d8446f97ee1ee8966b052daf4370b1f9f4
iamharshit990 <iamharshit990@gmail.com>
2026-09-22 12:18:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.41%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,13 +1,13 @@
 class Solution:
-    def spiralOrder(self, arr: list[list[int]]) -> list[int]:
-        ans = []
+    def spiralOrder(self, arr):
         n = len(arr)
         m = len(arr[0])
-        top = 0
+        ans=[]
+        top = 0 
         bottom = n-1
-        left = 0
+        left = 0 
         right = m-1
-        while (top<=bottom and left<=right):
+        while(left<=right and top <= bottom):
             for i in range (left , right+1):
                 ans.append(arr[top][i])
             top+=1
@@ -17,13 +17,10 @@ class Solution:
             if top<=bottom:
                 for i in range (right,left-1,-1):
                     ans.append(arr[bottom][i])
-                bottom -= 1
-            if(left<=right):
+                bottom-=1
+            if left<=right:
                 for i in range (bottom,top-1,-1):
                     ans.append(arr[i][left])
                 left+=1
-
-        return ans 
-
-
+        return ans
         
