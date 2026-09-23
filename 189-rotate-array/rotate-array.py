27208970acd8387024d1e73cb4fae11642ba1432
iamharshit990 <iamharshit990@gmail.com>
2026-09-23 20:53:01 +0530

    Time: 127 ms (32.49%) | Memory: 35.4 MB (10.60%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,16 +1,17 @@
 class Solution:
-    def rotate(self, nums: list[int], k: int) -> None:
+    def rotate(self, arr: list[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
+        n = len(arr)
         def rev(arr,i,j):
-            while(i<j):
+            while(i<=j):
                 arr[i] , arr[j] = arr[j] , arr[i]
                 i+=1
                 j-=1
         
         k=k%n
-        rev(nums,0,n-1)
-        rev(nums,0,k-1)
-        rev(nums,k,n-1)
+        
+        rev(arr,0,n-1)
+        rev(arr,0,k-1)
+        rev(arr,k,n-1)
