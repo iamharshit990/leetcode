cf319265beda772484b34829ec973330057f31c4
iamharshit990 <iamharshit990@gmail.com>
2026-09-25 13:41:53 +0530

    Time: 217 ms (90.76%) | Memory: 19.3 MB (70.91%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,32 +1,33 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s)==0 :
-            return ""
-        max_length = 0
-        index = [0]*2
-        def helper(s,left,right):
-            while left>=0 and right<len(s):
-                if s[left]!=s[right]:
+        if len(s) == 0:
+                return ""
+        max_length=0
+        index=[0]*2
+        def helper(s,i,j):
+            while i>=0 and j<len(s):
+                if s[i]!= s[j]:
                     break
-                left-=1
-                right+=1
-            return [left+1,right-1]
-
-        for i in range(len(s)):
-            odd_res = helper(s,i,i)
-            even_res=helper(s,i,i+1)
-            odd = odd_res[1] - odd_res[0]+1
-            even = even_res[1] - even_res[0]+1
-            if odd > even:
-                if odd>max_length:
-                    max_length=odd
-                    index[0] = odd_res[0]
-                    index[1] = odd_res[1]
+                i-=1
+                j+=1
+            return [i+1,j-1]
+        for i in range (0,len(s)):
+            odd_arr = helper(s,i,i)
+            even_arr = helper(s,i,i+1)
+            odd_length = odd_arr[1]-odd_arr[0]+1
+            even_length = even_arr[1]-even_arr[0]+1
+            if odd_length>even_length:
+                if odd_length>max_length:
+                    max_length = odd_length
+                    index[0] = odd_arr[0]
+                    index[1] = odd_arr[1]
             else:
-                if even>max_length:
-                    max_length=even
-                    index[0] = even_res[0]
-                    index[1] = even_res[1]
-
+                if even_length>max_length:
+                    max_length = even_length
+                    index[0] = even_arr[0]
+                    index[1] = even_arr[1]
         return s[index[0]:index[1]+1]
 
+
+
+        
